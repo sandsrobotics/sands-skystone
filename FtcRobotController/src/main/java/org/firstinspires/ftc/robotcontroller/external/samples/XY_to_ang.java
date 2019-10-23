a2278eda113eb676f21acc8a7ced2999bcc5bc17
sandsrobotics <senseandsoundrobotics@gmail.com>
2019-10-22 20:56:30 -0400

    XY angle proto COOL! all we need is to make it work in a real situation code: 102938293548927893A

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/XY_to_ang.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/XY_to_ang.java
@@ -1,65 +1,176 @@
+
 package org.firstinspires.ftc.robotcontroller.external.samples;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; //importation of data about the robot's hardware
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.hardware.adafruit.AdafruitBNO055IMU;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.util.ReadWriteFile;
-import java.util.regex.Pattern;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
-import org.firstinspires.ftc.robotcore.external.JavaUtil;
-import java.io.File;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
 import java.util.Locale;
 
 @TeleOp
+
 public class XY_to_ang extends LinearOpMode{
     private DcMotor leftMotor;
     private DcMotor rightMotor;
 
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
+
     @Override
+
     public void runOpMode() {
 
         leftMotor = hardwareMap.dcMotor.get("left");
         rightMotor = hardwareMap.dcMotor.get("right");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        composeTelemetry();
+        telemetry.update();
 
         waitForStart();
-        double DriveSpeed;
-        double DriveAngle;
-        double Rotate;
-        double v0;
-        double v1;
-        double v2;
-        double v3;
-        double maxValue;
 
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        double e = 0;
+        double target = 0;
+        double h;
+       // double DriveSpeed;
+       // double Rotate;
+       // double v0;
+       // double v1;
+       // double maxValue;
 
         while (opModeIsActive()) {
-            DriveSpeed = JavaUtil.minOfList(JavaUtil.createListWith(1, Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y)));
-            DriveAngle = Math.atan2(-gamepad1.left_stick_x, -gamepad1.left_stick_y) / Math.PI * 180;
-            Rotate = Math.pow(gamepad1.right_stick_x, 3);
-            DriveSpeed = Math.pow(DriveSpeed, 3);
-            v0 = DriveSpeed * (Math.cos(DriveAngle / 180 * Math.PI) - Math.sin(DriveAngle / 180 * Math.PI)) + Rotate;
-            v1 = DriveSpeed * (Math.cos(DriveAngle / 180 * Math.PI) + Math.sin(DriveAngle / 180 * Math.PI)) + Rotate;
 
-            telemetry.addData("r (magnitude)", DriveSpeed);
-            telemetry.addData("robotAngle", DriveAngle);
-            telemetry.addData("leftMotor", Math.round(v0 * 100) / 100);
-            telemetry.addData("rightMotor", Math.round(v1 * 100) / 100);
-
+            telemetry.addData("RightMotor", leftMotor.getPower());
+            telemetry.addData("LeftMotor", rightMotor.getPower());
+            telemetry.addData("This is E", e);
+            telemetry.addData("This is target", target);
             telemetry.update();
 
-            maxValue = JavaUtil.maxOfList(JavaUtil.createListWith(Math.abs(v0), Math.abs(v1), 1));
-            //leftMotor.setPower(v0 / maxValue);
-            //rightMotor.setPower(v1 / maxValue);
+            if ((Math.abs(gamepad1.left_stick_x)) + (Math.abs(gamepad1.left_stick_y)) > 0) {
+                    h = angles.firstAngle;
+                    target = Math.atan2(-gamepad1.left_stick_x, -gamepad1.left_stick_y) / Math.PI * 180;
 
+                    e = target - h;
+                    if (e > 180) {
+                        e = e - 360;
+                    } else if (e < -180) {
+                        e = e + 360;
+                    }
             }
         }
     }
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() {
+            @Override public void run() {
+                // Acquiring the angles is relatively expensive; we don't want
+                // to do that in each of the three items that need that info, as that's
+                // three times the necessary expense.
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+            }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func < String > () {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func < String > () {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func < String > () {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func < String > () {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func < String > () {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func < String > () {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func < String > () {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel * gravity.xAccel +
+                                        gravity.yAccel * gravity.yAccel +
+                                        gravity.zAccel * gravity.zAccel));
+                    }
+                });
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees) {
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}
+/*
+DriveSpeed = JavaUtil.minOfList(JavaUtil.createListWith(1, Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y)));
+DriveAngle = Math.atan2(-gamepad1.left_stick_x, -gamepad1.left_stick_y) / Math.PI * 180;
+Rotate = Math.pow(gamepad1.right_stick_x, 3);
+DriveSpeed = Math.pow(DriveSpeed, 3);
+v0 = DriveSpeed * (Math.cos(DriveAngle / 180 * Math.PI) - Math.sin(DriveAngle / 180 * Math.PI)) + Rotate;
+v1 = DriveSpeed * (Math.cos(DriveAngle / 180 * Math.PI) + Math.sin(DriveAngle / 180 * Math.PI)) + Rotate;
+
+telemetry.addData("r (magnitude)", DriveSpeed);
+telemetry.addData("robotAngle", DriveAngle);
+telemetry.addData("leftMotor", Math.round(v0 * 100) / 100);
+telemetry.addData("rightMotor", Math.round(v1 * 100) / 100);
+
+maxValue = JavaUtil.maxOfList(JavaUtil.createListWith(Math.abs(v0), Math.abs(v1), 1));
+leftMotor.setPower(v0 / maxValue);
+rightMotor.setPower(v1 / maxValue);
+ */
+
 
